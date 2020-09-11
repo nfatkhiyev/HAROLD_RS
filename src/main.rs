500cd9c1c7a91bdb97bf1c9a2ee7af497a587a3e
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2020-09-11 18:26:38 -0400

    async get s3 link

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use futures::executor::block_on;
 use ldap3::result::Result;
 use ldap3::{LdapConn, Scope, SearchEntry};
+use tokio::runtime::Runtime;
 
 mod requests;
 mod secrets;
@@ -12,7 +12,7 @@ fn main() -> Result<()> {
         requests::requests::get_uid(harold_secrets.get_nate_ibutton(), harold_secrets);
 
     let future_s3_link = requests::requests::get_s3_link(function_uid.unwrap(), harold_secrets);
-    block_on(future_s3_link);
+    Runtime::new().expect("asdfads").block_on(future_s3_link);
 
     let mut ldap = LdapConn::new(harold_secrets.get_ldap_server())?;
     let _res = ldap
