601578e86885526cc42934ac32d77fb03eba0db8
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2020-09-11 21:16:27 -0400

    async get s3 link

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,13 @@ use tokio::runtime::Runtime;
 mod requests;
 mod secrets;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() {
     let harold_secrets = secrets::secrets::initialized_secrets();
 
     let function_uid =
         requests::requests::get_uid(harold_secrets.get_nate_ibutton(), harold_secrets);
 
     let future_s3_link = requests::requests::get_s3_link(function_uid.unwrap(), harold_secrets);
-    Runtime::new().expect("asdfads").block_on(future_s3_link);
-
-    let mut ldap = LdapConn::new(harold_secrets.get_ldap_server())?;
-    let _res = ldap
-        .simple_bind(harold_secrets.get_ldap_dn(), harold_secrets.get_ldap_pw())?
-        .success()?;
-    let (rs, rsa) = ldap
-        .search(
-            "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu",
-            Scope::Subtree,
-            harold_secrets.get_nate_ibutton(),
-            vec!["uid"],
-        )?
-        .success()?;
-    for entry in rs {
-        println!("{:?}", SearchEntry::construct(entry).attrs["uid"][0]);
-    }
-    Ok(ldap.unbind()?)
+    println!("{:?}", future_s3_link.await.unwrap())
 }
